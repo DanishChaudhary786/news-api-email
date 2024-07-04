a65fac823d82138ec85f009e8b0286f344982ab2
DC <chaudharydanish024@gmail.com>
2024-07-04 17:07:58 +0530

    commit

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 import requests
 import send_email as se
 apikey = "34efc8e030c84c6789d6d229b1052883"
-# query = input("Enter your query: ")
-url = f"https://newsapi.org/v2/everything?q=apple&sortBy=publishedAt&apiKey={apikey}"
+query = "Danish Chaudhary"
+url = f"https://newsapi.org/v2/everything?q={query}&sortBy=publishedAt&apiKey={apikey}&language=en"
 
 request = requests.get(url)
 content = request.json()
-message =""
-for article in content["articles"]:
-    message = message + article["title"] + "\n" + article["description"] + 2*"\n"
+message ="Subject: Today's News \n"
+for article in content["articles"][0:20]:
+    if article['title'] is not None:
+        message = message + article["title"] + "\n" \
+                  + article["description"] +"\n" + \
+                  article["url"] + 2*"\n"
 
 message = message.encode("utf-8")
-se.sending_email("danish.chaudhary@wotnot.io", message)
+se.sending_email("chaudharydanish024@gmail.com", message)
